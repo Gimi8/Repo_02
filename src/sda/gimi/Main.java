7631f52e88a0dea64608f899f6bb1eef5b7201ec
Gimi8 <wojtek.gmiterek@o2.pl>
2020-05-17 10:40:46 +0200

    ProjektWGIT

--- src/sda/gimi/Main.java
+++ src/sda/gimi/Main.java
@@ -3,5 +3,8 @@ package sda.gimi;
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello");
+        for (int i = 0; i <5 ; i++) {
+            System.out.println("tak");
+        }
     }
 }
